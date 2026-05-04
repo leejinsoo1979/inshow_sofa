a1fd8174dd4ffdc2c27236ccea580d047e353e41
jinsoo <sbbc212@gmail.com>
2026-05-04 20:29:52 +0900

    그림자 살짝 어둡게 (opacity 0.45→0.55)

--- react-app/src/scene/Scene.jsx
+++ react-app/src/scene/Scene.jsx
@@ -21,7 +21,7 @@ function Floor() {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
       <planeGeometry args={[18, 14]} />
-      <shadowMaterial color={0x000000} opacity={0.45} transparent />
+      <shadowMaterial color={0x000000} opacity={0.55} transparent />
     </mesh>
   );
 }
